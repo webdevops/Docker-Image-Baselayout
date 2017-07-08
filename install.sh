1a2f475c91586d23a369c74e63c221edd7288019
Markus Blaschke <mblaschke82@gmail.com>
2017-07-08 18:05:49 +0200

    Install to custom target

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
+TARGET_DIR="/"
+
+if [ "$#" -eq 1 ]; then
+    TARGET_DIR="$1"
+fi
+
 umask 0022
 
 if [ -f "/sbin/apk" ]; then
@@ -16,6 +22,5 @@ else
     wget -O /tmp/baselayout.tar.gz https://github.com/webdevops/Docker-Image-Baselayout/archive/master.tar.gz
 fi
 
-
-tar --no-same-permissions --strip-components=2 -xf /tmp/baselayout.tar.gz  -C /
+tar --no-same-permissions --strip-components=2 -xf /tmp/baselayout.tar.gz  -C "$TARGET_DIR"
 rm -f /tmp/baselayout.tar.gz
